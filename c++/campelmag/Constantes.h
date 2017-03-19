dbd81c99ef1bd48c3540d693fc8e59d7cf61192c
fzappa <alanfr.cefetrj@gmail.com>
2017-03-18 22:19:17 -0300

    build

---
+++ c++/campelmag/Constantes.h
@@ -0,0 +1,45 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+// chamada para complexo
+
+//Tensão da Linha (V)
+constexpr double V {765e3};
+
+//Potência Transmitida (W)
+constexpr double P {1.2e9};
+constexpr double Ir {P/V};
+
+//Angulos das fases (graus)
+const Eigen::Vector3d AngulosABC {120,0,-120};
+
+//Corrente com a defasagem
+const Eigen::Vector3cd* Itemp {Iri(Ir,AngulosABC)};
+const Eigen::Vector3cd I {*Itemp};
+
+//Caracteristicas Mecânicas
+const vector<double> LinhaDeMedicao {-80.0, 80.0, 0.5, 1.5}; //[Xmin Xmax Passo Altura]
+constexpr double M {0.87};
+constexpr double Hmax {25.0}; //#[m]
+constexpr double FLECHA {8.63}; //#[m]
+constexpr double EspacSubCond {0.457}; // # [m]
+constexpr double Dcabo {0.02959};
+constexpr double Ncond {4.0};
+constexpr double h0 {Hmax-FLECHA-EspacSubCond/2.0};
+const Eigen::Vector3d H {h0, h0, h0}; //#Altura mínima do cabo no feixe
+const Eigen::Vector3d D {-14.34, 0.0, 14.34};
+
+//Passando por referencia, para facilitar a leitura
+const double& minimo {((LinhaDeMedicao[1]-LinhaDeMedicao[0])/LinhaDeMedicao[2])+1};
+const double& passo {LinhaDeMedicao[0]};
+const double& maximo {LinhaDeMedicao[0]+LinhaDeMedicao[2]*((LinhaDeMedicao[1]-LinhaDeMedicao[0])/LinhaDeMedicao[2])};
+
+
+
+
+#endif
